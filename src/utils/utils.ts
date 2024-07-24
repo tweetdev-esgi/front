4486cd77e2c1547d38801bf6bdac5fdccb94b249
paschyz <paschy2002@gmail.com>
2024-07-24 22:48:42 +0200

    chore: Update API_BASE_URL and FRONT_BASE_URL to use string type

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,5 @@
-export const API_BASE_URL = process.env.API_BASE_URL;
-export const FRONT_BASE_URL = process.env.FRONT_BASE_URL;
+export const API_BASE_URL = process.env.API_BASE_URL as string;
+export const FRONT_BASE_URL = process.env.FRONT_BASE_URL as string;
 
 export const convertTimestampToMonthYear =(timestamp: string): string =>{
     const date = new Date(timestamp);
